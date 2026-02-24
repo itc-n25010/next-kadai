c859a4b162e54e95383ac5946aaa531de9f6f43f
itc-n25010 <n25010@std.it-college.ac.jp>
2026-02-24 09:20:26 +0900

    学校ロゴ修正

--- app/_components/SchoolSection/index.tsx
+++ app/_components/SchoolSection/index.tsx
@@ -6,10 +6,10 @@ type Props = {
 };
 
 const logoMap: Record<string, string> = {
-  アビドス高等学校: "/images/abydos.png",
-  ゲヘナ学園: "/images/gehenna.png",
-  ミレニアムサイエンススクール: "/images/millennium.png",
-  トリニティ総合学園: "/images/trinity.png",
+  アビドス高等学校: "/logo/abydos.png",
+  ゲヘナ学園: "/logo/gehenna.png",
+  ミレニアムサイエンススクール: "/logo/millennium.png",
+  トリニティ総合学園: "/logo/trinity.png",
 };
 
 export default function SchoolSection({ school }: Props) {
